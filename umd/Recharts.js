e941bf31601ebdd72e3fc15919db08b3f0afe93f
Susana Costa <susana.costa@quantumblack.com>
2016-10-05 17:05:22 +0100

    Revert "scatter charts"
    This reverts commit c6a6efe01947bab7b0f5e5da76de8f2f3459c6dd.

--- umd/Recharts.js
+++ umd/Recharts.js
@@ -33747,15 +33747,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	      var children = this.props.children;
 
 	      var tooltipItem = (0, _ReactUtils.findChildByType)(children, _Tooltip2.default);
+
+	      if (!tooltipItem || !tooltipItem.props.cursor || !this.state.isTooltipActive) {
+	        return null;
+	      }
+
 	      var _state = this.state;
 	      var isTooltipActive = _state.isTooltipActive;
 	      var activeItem = _state.activeItem;
 	      var activeTooltipCoord = _state.activeTooltipCoord;
-
-
-	      if (!tooltipItem || !tooltipItem.props.cursor || !isTooltipActive) {
-	        return null;
-	      }
 
 	      var viewBox = {
 	        x: offset.left,
